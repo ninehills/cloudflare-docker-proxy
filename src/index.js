63e374fd2ce72959b417c4acf6610a76f2c5f5b3
Tao Yang <swulling@gmail.com>
2024-07-17 14:44:56 +0800

    update domain to  ninehills.tech

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.ninehills.tech": dockerHub,
+  "quay.ninehills.tech": "https://quay.io",
+  "gcr.ninehills.tech": "https://gcr.io",
+  "k8s-gcr.ninehills.tech": "https://k8s.gcr.io",
+  "k8s.ninehills.tech": "https://registry.k8s.io",
+  "ghcr.ninehills.tech": "https://ghcr.io",
+  "cloudsmith.ninehills.tech": "https://docker.cloudsmith.io",
+  "ecr.ninehills.tech": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.ninehills.tech": dockerHub,
 };
 
 function routeByHosts(host) {
